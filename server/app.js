d1749f1733aa8710e194912bc42dca3c185b318e
Nursat Bakyt <nursat@MacBook-Pro-Nursat.local>
2022-11-24 04:10:35 +0600

    ad

--- server/app.js
+++ server/app.js
@@ -14,19 +14,14 @@ const app = express()
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 app.use(cors())
-app.use(bodyParser.json({limit:"30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit:"30mb", extended: true}))
+app.use(bodyParser.json({limit:"60mb", extended: true}))
+app.use(bodyParser.urlencoded({limit:"60mb", extended: true}))
+app.use("/safe-phone", safePhoneRoutes)
 app.use('/orders', ordersRoutes)
 app.use('/requests', requestsRoutes)
 app.use("/user", userRoutes)
-app.use("/safe-phone", safePhoneRoutes)
-
-
-// mongoose.connect("mongodb+srv://user:qwerty123@cluster0.bfy5yoo.mongodb.net/?retryWrites=true&w=majority", {
-//     useNewUrlParser: true, 
-//     useUnifiedTopology: true 
-// }).then(() => app.listen(5100, () => console.log("Server launched")))
 
 mongoose.connect("mongodb://127.0.0.1:27017/?retryWrites=true&w=majority", {
     useNewUrlParser: true, 
