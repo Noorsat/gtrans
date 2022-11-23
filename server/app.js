a26e4eb38d6446ef7726da3d3dd48782a4110379
Nursat Bakyt <nursat@MacBook-Pro-Nursat.local>
2022-11-24 05:17:18 +0600

    dfds

--- server/app.js
+++ server/app.js
@@ -28,6 +28,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/?retryWrites=true&w=majority", {
     useUnifiedTopology: true 
 }).then(() => app.listen(5100, () => console.log("Server launched")))
 
+
 // mongoose.connect("mongodb+srv://user:qwerty123@cluster0.bfy5yoo.mongodb.net/?retryWrites=true&w=majority", {
 //     useNewUrlParser: true, 
 //     useUnifiedTopology: true 
