24dabc618a12d2353056a2a43ba1f3d6c09f2d35
Nursat Bakyt <nursat@MacBook-Pro-Nursat.local>
2022-11-25 20:16:33 +0600

    adsd

--- server/routes/safePhoneUser.js
+++ server/routes/safePhoneUser.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { login, register, sendEmailPhoto } from '../controllers/safePhoneUser.js';
 import multer from 'multer';
-import path from 'path'
+import path from 'path';
+import nodemailer from 'nodemailer';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -22,7 +23,40 @@ router.post("/register", register);
 router.post("/login", login);
 
 router.post("/send-email-in-error", upload.single('image'), (req, res) => {
-    res.send("Image Uploaded")
+    try{
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: 'itsnursat@gmail.com',
+              pass: 'pdlfyedtkldiqrik'
+            }
+          });
+          
+          var mailOptions = {
+            from: 'itsnursat@gmail.com',
+            to: 'itsnursat@gmail.com',
+            subject: 'asda',
+            text: 'sadad',
+            attachments:[
+                {
+                  fileName: 'index.jpg',
+                  path: req.file.path
+                }
+              ]
+          };
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+          res.status(200).json({
+            message:"Email sent: " + email
+          })
+    }catch (error){
+        res.status(400).json({error: error.message})
+    }
 });
 
 export default router;
