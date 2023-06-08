6ecc09210a190d03b61174b8a0ffbb9f86266544
Nursat Bakyt <nursat@MacBook-Pro-Nursat.local>
2023-06-09 02:47:17 +0600

    fix: link

--- client/http/axios.js
+++ client/http/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const myAxios = axios.create({
-    baseURL:"https://gtrans-ffxd.onrender.com/"
+    baseURL:"https://backend.gtrans.kz/"
 })
 
 export default myAxios;
