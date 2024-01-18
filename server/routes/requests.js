6636953a610446ab923a547d4d259b8eeb08b8e9
Nursat Bakyt <itsnoorsat@MacBook-Air-Nursat.local>
2024-01-18 21:02:22 +0600

    feat: add request for get requests by user id

--- server/routes/requests.js
+++ server/routes/requests.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createRequest, getRequests, getRequestsByOrder } from '../controllers/request.js';
+import { createRequest, getRequests, getRequestsByOrder, getRequestsByUserId } from '../controllers/request.js';
 
 const router = express.Router();
 
 router.get("/", getRequests)
+router.get("/my", getRequestsByUserId)
 router.get("/:id", getRequestsByOrder)
 router.post("/", createRequest)
 
